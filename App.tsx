637492441f7ba85acff58d234b19da76acf572af
Hoshi <masa317dry@gmail.com>
2023-06-18 14:44:31 +0900

    📦 パッケージの追加

--- App.tsx
+++ App.tsx
@@ -1,28 +1,39 @@
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NativeBaseProvider } from 'native-base';
+import { NavigationContainer } from "@react-navigation/native"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { NativeBaseProvider } from "native-base"
+import { View, Text } from "react-native"
+import { RecoilRoot } from "recoil"
+import { SWRConfig } from "swr"
 
 function HomeScreen() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Home Screen</Text>
     </View>
-  );
+  )
 }
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator()
 
 function App() {
   return (
-    <NativeBaseProvider>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </NativeBaseProvider>
-  );
+    <RecoilRoot>
+      <SWRConfig
+        value={{
+          fetcher: (resource, init) =>
+            fetch(resource, init).then((res) => res.json()),
+        }}
+      >
+        <NativeBaseProvider>
+          <NavigationContainer>
+            <Stack.Navigator>
+              <Stack.Screen name="Home" component={HomeScreen} />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </NativeBaseProvider>
+      </SWRConfig>
+    </RecoilRoot>
+  )
 }
 
-export default App;
+export default App
--- README.md
+++ README.md
@@ -19,3 +19,27 @@ yarn add @react-navigation/native-stack @react-navigation/stack
 yarn add native-base
 
 expo install react-native-svg
+
+npx expo install expo-camera
+
+expo install expo-image-picker
+
+yarn add react-native-snap-carousel
+
+yarn add -D @types/react-native-snap-carousel
+
+yarn add lodash
+
+yarn add -D @types/lodash
+
+yarn add recoil
+
+yarn add react-native-reanimated
+
+yarn add react-native-modal
+
+yarn add react-native-scrollable-tab-view
+
+yarn add -D @types/react-native-scrollable-tab-view
+
+yarn add swr
