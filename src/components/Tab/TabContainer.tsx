aa1191cd75297775fead4efb12f6b8eca8a13531
daisuketujita <daisuketujita@gmail.com>
2023-09-17 22:41:35 +0900

    タブ切り替えが遅い問題を修正

--- src/components/Tab/TabContainer.tsx
+++ src/components/Tab/TabContainer.tsx
@@ -1,15 +1,6 @@
-import {
-  Box,
-  Pressable,
-  ScrollView,
-  useColorModeValue,
-  Text,
-  View,
-  Center,
-  Spinner,
-} from "native-base"
+import { Box, Pressable, ScrollView, Text, View } from "native-base"
 import React, { useCallback, useEffect, useState } from "react"
-import { Dimensions, Animated, StyleSheet } from "react-native"
+import { Dimensions, StyleSheet } from "react-native"
 import { SceneMap, TabView } from "react-native-tab-view"
 
 interface Tab {
@@ -28,7 +19,6 @@ export const TabContainer: React.FC<TabContainerProps> = ({
   tabs,
   activeIndex,
 }) => {
-  const [height, setHeight] = useState<number>(0)
   const [index, setIndex] = useState<number>(activeIndex ?? 0)
   const [routes, setRoutes] = useState(
     tabs.map((tab) => ({
@@ -36,157 +26,127 @@ export const TabContainer: React.FC<TabContainerProps> = ({
       title: tab.title,
     }))
   )
+  const [heights, setHeights] = useState<number[]>(Array(tabs.length).fill(0))
+
   useEffect(() => {
-    const routes = tabs.map((tab) => ({
-      key: tab.title,
-      title: tab.title,
-    }))
-    setRoutes(routes)
-    setHeight(0)
+    setRoutes(
+      tabs.map((tab) => ({
+        key: tab.title,
+        title: tab.title,
+      }))
+    )
+    setHeights(Array(tabs.length).fill(0))
   }, [tabs])
 
-  const sceneMap: { [key: string]: React.FC } = {}
-  tabs.forEach((tab) => {
-    sceneMap[tab.title] = tab.content
-  })
-  const renderScene = SceneMap(sceneMap)
+  const renderScene = useCallback(() => {
+    const sceneMap: { [key: string]: React.FC } = {}
+    tabs.map((tab) => {
+      sceneMap[tab.title] = tab.content
+    })
+    return SceneMap(sceneMap)
+  }, [tabs])
 
-  const renderTabBar = useCallback(
-    (props: any) => {
-      return (
-        <Box flexDirection="row">
-          {props.navigationState.routes.map((route: any, i: number) => {
-            const color =
-              index === i
-                ? useColorModeValue("#000", "#e5e5e5")
-                : useColorModeValue("#1f2937", "#a1a1aa")
-            const borderColor =
-              index === i
-                ? "cyan.500"
-                : useColorModeValue("coolGray.200", "gray.400")
-            return (
-              <Box
-                key={i}
-                borderBottomWidth="3"
-                borderColor={borderColor}
-                flex={1}
-                alignItems="center"
-                p="0"
+  const renderTabBar = useCallback(() => {
+    return (
+      <Box flexDirection="row">
+        {routes.map((route: any, i: number) => {
+          return (
+            <Box
+              key={i}
+              borderBottomWidth="3"
+              borderColor={index === i ? "cyan.500" : "coolGray.200"}
+              flex={1}
+              alignItems="center"
+              p="0"
+            >
+              <Pressable
+                onPress={() => {
+                  setIndex(i)
+                }}
               >
-                <Pressable
-                  onPress={() => {
-                    handleOnIndexChange(i)
-                  }}
-                >
-                  <Box
-                    minW="100%"
-                    minH={10}
-                    display="flex"
-                    alignItems="center"
-                    justifyContent="center"
-                  >
-                    <Animated.Text
-                      style={{
-                        color,
-                      }}
-                    >
-                      {route.title}
-                    </Animated.Text>
-                  </Box>
-                </Pressable>
-              </Box>
-            )
-          })}
-        </Box>
-      )
+                <Box style={styles.tabBarBox}>
+                  <Text>{route.title}</Text>
+                </Box>
+              </Pressable>
+            </Box>
+          )
+        })}
+      </Box>
+    )
+  }, [tabs, index])
+
+  const setContainerHeight = useCallback(
+    (containerHeight: number, index: number) => {
+      // 高さを一度も計算していなければ計算して保持する
+      if (heights[index] === 0 && containerHeight !== 0) {
+        const newHeights = [...heights]
+        newHeights[index] = containerHeight
+        setHeights(newHeights)
+      }
     },
-    [tabs, index]
+    [heights]
   )
 
-  const setContainerHeight = (containerHeight: number) => {
-    //console.log(new Date().getTime() + " setContainerHeight=" + containerHeight)
-    if (height === 0 && containerHeight !== 0) {
-      const newHeight =
-        containerHeight < windowHeight ? windowHeight : containerHeight + 30
-      setHeight(newHeight)
-    }
-  }
-  const handleOnIndexChange = (index: number) => {
-    //console.log(new Date().getTime() + " onIndexChange index=" + index)
-    setIndex(index)
-    setHeight(0)
+  if (heights[index] === 0) {
+    //console.log(new Date().getTime() + " --- dummy render ---")
+    /* TODO
+    react-native-tab-viewの不具合でTabViewの高さが自動計算されないため、
+    TabViewを使わずにコンテンツを表示させて高さを取得し、再びTabViewを使って再描画している
+    */
+    return (
+      <View style={styles.container}>
+        <ScrollView
+          onLayout={(event) => {
+            // ダミーを表示して高さを取得
+            const containerHeight = event.nativeEvent.layout.height
+            setContainerHeight(containerHeight, index)
+          }}
+          style={{ display: "flex" }}
+        >
+          <>
+            <Box flexDirection="row">
+              {routes.map((route: any, i: number) => {
+                return (
+                  <Box
+                    key={i}
+                    borderBottomWidth="3"
+                    borderColor={index === i ? "cyan.500" : "coolGray.200"}
+                    flex={1}
+                    alignItems="center"
+                    p="0"
+                  >
+                    <Box style={styles.tabBarBox}>
+                      <Text>{route.title}</Text>
+                    </Box>
+                  </Box>
+                )
+              })}
+            </Box>
+            {tabs[index].content({})}
+          </>
+        </ScrollView>
+      </View>
+    )
   }
 
+  //console.log(new Date().getTime() + " --- render ---")
   return (
     <View style={styles.container}>
-      <ScrollView style={{ flex: 1 }}>
+      <ScrollView style={{ display: "flex" }}>
         <TabView
           navigationState={{
             index,
             routes,
           }}
-          renderScene={renderScene}
+          renderScene={renderScene()}
           renderTabBar={renderTabBar}
-          onIndexChange={handleOnIndexChange}
+          onIndexChange={(index) => setIndex(index)}
           initialLayout={{ width: Dimensions.get("window").width }}
-          style={{ height }}
+          style={{
+            height: heights[index] === 0 ? windowHeight : heights[index],
+          }}
         />
       </ScrollView>
-
-      {/* TODO
-        react-native-tab-viewの不具合でTabViewの高さが自動計算されないため、
-        TabViewを使わずにコンテンツを表示させて高さを取得し、再びTabViewを使って再描画している 
-      */}
-      <ScrollView
-        onLayout={(event) => {
-          // 高さを取得
-          const containerHeight = event.nativeEvent.layout.height
-          setContainerHeight(containerHeight)
-        }}
-        style={{ display: height === 0 ? "flex" : "none" }}
-      >
-        <Box flexDirection="row">
-          {tabs.map((tab, tabIndex) => (
-            <Box
-              key={tab.title}
-              borderBottomWidth="3"
-              borderColor={
-                index === tabIndex
-                  ? "cyan.500"
-                  : useColorModeValue("coolGray.200", "gray.400")
-              }
-              flex={1}
-              alignItems="center"
-              p="0"
-            >
-              <Box
-                minW="100%"
-                minH={10}
-                display="flex"
-                alignItems="center"
-                justifyContent="center"
-              >
-                <Text>{tab.title}</Text>
-              </Box>
-            </Box>
-          ))}
-        </Box>
-        {tabs.map((tab, tabIndex) => (
-          <View
-            key={tab.title}
-            style={{ display: index === tabIndex ? "flex" : "none" }}
-          >
-            <tab.content />
-          </View>
-        ))}
-      </ScrollView>
-      {height === 0 && (
-        <Center style={styles.highLight}>
-          <Spinner size="lg" top={20}>
-            Loading
-          </Spinner>
-        </Center>
-      )}
     </View>
   )
 }
@@ -195,7 +155,7 @@ const styles = StyleSheet.create({
     position: "relative",
     flex: 1,
   },
-  highLight: {
+  backdrop: {
     flex: 1,
     justifyContent: "flex-start",
     position: "absolute",
@@ -205,5 +165,12 @@ const styles = StyleSheet.create({
     height: "100%",
     backgroundColor: "white",
     opacity: 0.3,
+  },
+  tabBarBox: {
+    minWidth: "100%",
+    minHeight: 40,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
   },
 })
